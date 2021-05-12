d0c69224962e128b8370d870e35023c9bec1519a
Anthony Almeida <Anthonyl.almeida@gmail.com>
2021-05-12 19:23:51 -0300

    first commit

---
+++ back-end/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '686493cb-56ce-452e-8be7-97506c741275'
+};
---
+++ front-end/pages/register.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+function Login() {
+  return (
+    <div>
+      <div>
+        <h1>cadastro doido</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
